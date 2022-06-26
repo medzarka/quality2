79eb809989d9471edd3b4c4db710a88e64eea9db
mzarka <medzarka@gmail.com>
2022-06-26 23:50:39 +0300

    make uKKU2 working on namecheap.com 23

--- update_passenger_wsgi.py
+++ update_passenger_wsgi.py
@@ -2,7 +2,7 @@ import os
 import sys
 import environ
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 if 'env_file' in os.environ.keys():
     env_file = os.path.join(BASE_DIR, 'env', os.getenv('env_file'))
