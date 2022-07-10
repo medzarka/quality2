9040513288b118a3f8b713b6e3ed5a28ea0e4ecd
mzarka <medzarka@gmail.com>
2022-07-10 19:02:16 +0300

    make uKKU2 working on namecheap.com 44. Now change __str__ to __unicode__

--- quality2/settings.py
+++ quality2/settings.py
@@ -150,7 +150,7 @@ if env.str('DATABASE_URL', default=''):
         'default': env.db(),
     }
 
-    DATABASES['default']['OPTION'] = {
+    DATABASES['default']['OPTIONS'] = {
         'init_command': "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1",
         'charset': 'utf8mb4',
         "autocommit": True,
