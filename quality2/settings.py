98b7939f2a177320b7680f925a3277539161c491
mzarka <medzarka@gmail.com>
2022-08-21 12:27:05 +0100

    make uKKU2 working on namecheap.com. 18 August 2022. Work with db logging 31.

--- quality2/settings.py
+++ quality2/settings.py
@@ -143,23 +143,22 @@ WSGI_APPLICATION = 'quality2.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 
-if DEBUG is False:#env.str('DATABASE_URL', default=''):
+if DEBUG:#env.str('DATABASE_URL', default=''):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'django_dev.sqlite3',
+        },
+    }
+else:
     DATABASES = {
         'default': env.db(),
     }
 
     DATABASES['default']['OPTIONS'] = {
-    'init_command': "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1",
-    'charset': 'utf8mb4',
-    "autocommit": True,
-    }
-
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': SITE_DATA_DIR.path('db')('django.sqlite3'),
-        },
+        'init_command': "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1",
+        'charset': 'utf8mb4',
+        "autocommit": True,
     }
 
 print(f'DATABASES --> {DATABASES}')
