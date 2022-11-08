21af6585cd87f887a9136753a1b4dc311d0d8922
mzarka <medzarka@gmail.com>
2022-11-08 16:08:51 +0300

    add PRODUCTION in configuration the dfault place where to put all extrnal data is .uKKU2 in the home directory.
    MySQL issue

--- quality2/settings.py
+++ quality2/settings.py
@@ -160,7 +160,7 @@ else:
     }
 
     DATABASES['default']['OPTIONS'] = {
-        'init_command': "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1",
+        'init_command': "SET storage_engine='INNODB', sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1",
         'charset': 'utf8mb4',
         "autocommit": True,
     }
